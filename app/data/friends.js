53ad3ce86f52e6bd92707832302fccd09eb562f1
Emily Dalton <emilydalton@dhcp-10-105-17-25.wireless.northwestern.private>
2019-01-15 10:00:03 -0600

    api routes connected and default object in friends.js

---
+++ app/data/friends.js
@@ -0,0 +1,8 @@
+const dogFriends = [{
+    name: "Default Fido",
+    image: "http://cdn.akc.org/content/article-body-image/shiba_inu_cute_puppies.jpg",
+    responseArr:["1", "1", "1", "1", "1", "1", "1", "1", "1", "1", ]
+    
+    }]
+    //module.exports makes the dogFriends object accessible to other files using require
+    module.exports = dogFriends;
