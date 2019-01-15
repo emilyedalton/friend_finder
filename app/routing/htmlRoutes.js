f20804a27b4a8b1de5b5a8ccfa09e83d68cc2c6a
Emily Dalton <emilydalton@dhcp-10-105-17-25.wireless.northwestern.private>
2019-01-15 12:45:00 -0600

    jquery submit button working

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -15,13 +15,13 @@ module.exports = function(app) {
   // In each of the below cases the user is shown an HTML page of content
   // ---------------------------------------------------------------------------
 
-  app.get("/tables", function(req, res) {
+  app.get("/home", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/home.html"));
   });
 
-//   app.get("/reserve", function(req, res) {
-//     res.sendFile(path.join(__dirname, "../public/reserve.html"));
-//   });
+  app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
+  });
 
   // If no matching route is found default to home
   app.get("*", function(req, res) {
