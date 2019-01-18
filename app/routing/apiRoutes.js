337cfb0c668f4e906578dcf8646b45dcd1d5db31
Emily Dalton <emilydalton@Emilys-MacBook-Air.local>
2019-01-18 16:16:31 -0600

    forgot to add '3' to dropdown selections

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,6 +2,7 @@
 // The dogFrends variable is from the friends.js file that we exported the dogfriends array of objects out of the friends.js file
 
 var dogFriends = require("../data/friends.js");
+// requiring the array-sort npm package which sorts an array based on a property https://www.npmjs.com/package/array-sort
 var arraySort = require('array-sort');
 
 
@@ -13,16 +14,17 @@ module.exports = function(app) {
 
   // API GET Requests
   // Below code handles when users "visit" a page.
-  // In each of the below cases when a user visits a link
-  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-  // ---------------------------------------------------------------------------
 
   app.get("/api/friends", function(req, res) {
     res.json(dogFriends);
   });
 
 
-  
+  // API POST Requests
+  // Post routes are used anytime data is added to something, 
+  //in this case our form responses are being added to the dogFriends array of objects in Friends.js
+  // However, before we add that data, we have to use it and the other objects in dogFriends to make another array of objects that will 
+  // be the "big reveal" in the app, who your dog friend is. 
 app.post("/api/friends", function(req, res) {
     // Our "server"  will respond to requests and get the current user's info
     // req.body is available since we're using the body parsing middleware this variable will get the the object array from the friends.js file
@@ -44,15 +46,14 @@ for (var i = 0; i < dogFriends.length; i++) {
 
    }
    //Then it will put each object with the total difference into the bestdog array of objects
-   bestDog.push({ name: dogFriends[i].name, image: dogFriends[i].image, totalDifference: Math.abs(results) });
+   bestDog.push({ name: dogFriends[i].name, image: dogFriends[i].image, matchDiff: Math.abs(results) });
 
-}//end outer for loop
+}
 
-//reference: https://www.npmjs.com/package/array-sort
-//this npm package allows you to sort the array of object.
-//The first argument would is the array of objects that needs to be sorted, in this case, the bestDog object that was created out of the date from the api/friends.js file
-//be the array of objects and the second argument would be your property name to //be sorted in ascending order
-arraySort(bestDog, 'totalDifference');
+// Here is where the array-sort module comes into play. The first argument is the array of objects that needs to be sorted,
+// in this case, the bestDog object that was created out of the data from the api/friends.js file
+//The first argument is the array of objects and the second argument is the name of the property that wil be sorted
+arraySort(bestDog, 'matchDiff');
 
 
 //insert current user to the existing tableData
@@ -60,15 +61,17 @@ dogFriends.push(currentDog);
 
 //first element of this array of object has the least total difference after sorting in //ascending order
 //which is the closest match
-//return res.json(storeTotaldifference[0]);
 
 console.log(dogFriends);
-
-console.log("Match:"+bestDog[0].totalDifference );
-console.log("name:"+bestDog[0].name +" image: "+ bestDog[0].image );
+console.log(`Match: ${bestDog[0].matchDiff}` );
+console.log( `Name: ${bestDog[0].name} Image: ${bestDog[0].image}`);
       
      // returns the object we created 
        res.json(bestDog[0]);
     });
    
 }
+
+//Activities referenced 
+//'HOT RESTAURANT', Trilogy Education Inc. https://nu.bootcampcontent.com/NU-Coding-Bootcamp/NUEVA201809FSF4/tree/master/activities/13-week/16-HotRestaurant/Solved/data
+//Philip Loy's code reference document was also helpful in understanding how we needed to manipulate the array of objects wihtin the post function.
